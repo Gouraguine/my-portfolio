fcadaecf7ed7a110066275de0e551ea87d47cac0
Salim Gouraguine <gouraguinesalim@gmail.com>
2025-12-02 00:14:25 +0000

    Update: navbar toggle, routing, responsive design, skills and contact sections

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/', // Ensure this matches your GitHub repository name
   plugins: [
     react({
       babel: {
